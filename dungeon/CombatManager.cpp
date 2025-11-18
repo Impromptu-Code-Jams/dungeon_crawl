4a182d48c8d99ec627c85b732e436514273ead3f
MemeDing <memeding1@gmail.com>
2025-11-18 13:08:01 -0500

    Create test for display

--- dungeon/CombatManager.cpp
+++ dungeon/CombatManager.cpp
@@ -1,1 +1,10 @@
-// Do stuff here!
+void goto_xy(int x, int y)
+{
+    std::cout << "\033[" << (y + 1) << ";" << (x + 1) << "H";
+}
+
+void CombatManager::display(int x, int y)
+{
+    goto_xy(x, y);
+    std::cout << "Hello World!";
+}
