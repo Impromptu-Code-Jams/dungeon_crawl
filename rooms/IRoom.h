91832f3328c1374e1ef7e6a6eb02b89b70d099ef
Joey DiGiorgio <digiorgiojoey@gmail.com>
2025-11-18 09:34:32 -0500

    Modified the 'IRoom.h' interface.

--- rooms/IRoom.h
+++ rooms/IRoom.h
@@ -8,9 +8,9 @@ class IRoom
         virtual ~IRoom() {}
         virtual std::string const name() const = 0;
         virtual std::vector<std::string> getTileMap() const = 0;
-//        virtual void onEnter(Player& player, Dungeon& dungeon) = 0;
-//        virtual void onPlayerStep(Player& player, Dungeon& dungeon) = 0;
-//        virtual void onTick(Player& player, Dungeon& dungeon) = 0;
+//        virtual void onEnter(Player& player, RoomApi& api) = 0;
+//        virtual void onPlayerStep(Player& player, RoomApi& api) = 0;
+//        virtual void onTick(Player& player, RoomApi& api) = 0;
 //        virtual bool isComplete() const = 0;        
 };
 
