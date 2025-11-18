6f3ec4a2d12922dee7c3d019e85b5a3617521b29
Christian Nicewaner <nicewanerc@wittenberg.edu>
2025-11-18 14:17:56 -0500

    added room api outputs for combat commands, combat starts in true

--- dungeon/CombatManager.cpp
+++ dungeon/CombatManager.cpp
@@ -1,25 +1,25 @@
 #include "CombatManager.h"
 
-bool CombatManager::handleInput(std::string input)
+bool CombatManager::handleInput(std::string input, RoomApi& api)
 {
 	bool inCombat = true;
 
 	if (input == "A" || input == "a")
 	{
-
-
+		api.showMessage("You attacked!");
 	}
 	else if (input == "D" || input == "d")
 	{
-
+		api.showMessage("You defended!");
 	}
 	else if (input == "M" || input == "m")
 	{
-
+		api.showMessage("You cast a spell!");
 	}
 	else if (input == "I" || input == "i")
 	{
-
+		api.showMessage("You used an item!");
+		inCombat = false;
 	}
 
 	return inCombat;
