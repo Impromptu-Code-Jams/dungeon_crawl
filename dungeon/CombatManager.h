90e2890c0e5581fa15bf7a9ec6ff564fdb07b5d8
Christian Nicewaner <nicewanerc@wittenberg.edu>
2025-11-18 13:41:39 -0500

    added combat input and combat manager stuff

--- dungeon/CombatManager.h
+++ dungeon/CombatManager.h
@@ -1,6 +1,11 @@
+#pragma once
+
 #include <vector>
+#include <string>
 
 class CombatManager
 {
-    
-}
+public:
+	CombatManager() = default;
+	bool handleInput(std::string input);
+};
--- dungeon/DungeonEngine.cpp
+++ dungeon/DungeonEngine.cpp
@@ -8,41 +8,44 @@
 #include "GetCh.h"
 
 DungeonEngine::DungeonEngine() :
-    running(false),
-    quit(false),
-    currentRoomIndex(0),
-    hasInput(false),
-    inputBuffer()
-{}
+	running(false),
+	quit(false),
+	inCombat(false),
+	currentRoomIndex(0),
+	hasInput(false),
+	inputBuffer()
+{
+	combatManager = std::make_unique<CombatManager>();
+}
 
 void DungeonEngine::addRoom(std::unique_ptr<IRoom> room)
 {
-    rooms.push_back(std::move(room));
+	rooms.push_back(std::move(room));
 }
 
 void DungeonEngine::start()
 {
-    if(rooms.empty())
-    {
-        std::cout << "No rooms, no dungeon..." << std::endl;
-        return;
-    }
+	if (rooms.empty())
+	{
+		std::cout << "No rooms, no dungeon..." << std::endl;
+		return;
+	}
 
-    // The starting room is always at index 0.
-    enterRoom(0);
+	// The starting room is always at index 0.
+	enterRoom(0);
 
-    running = true;
-    std::thread t1(&DungeonEngine::loop, this);
-    std::thread t2(&DungeonEngine::getInput, this);
+	running = true;
+	std::thread t1(&DungeonEngine::loop, this);
+	std::thread t2(&DungeonEngine::getInput, this);
 
-    t1.join();
-    t2.join();
+	t1.join();
+	t2.join();
 }
 
 void DungeonEngine::enterRoom(int roomIndex)
 {
-    currentRoomIndex = roomIndex;
-    rooms[currentRoomIndex]->onEnter(roomApi);
+	currentRoomIndex = roomIndex;
+	rooms[currentRoomIndex]->onEnter(roomApi);
 }
 
 void DungeonEngine::getInput()
@@ -51,153 +54,160 @@ void DungeonEngine::getInput()
     {
         inputBuffer = getSingleChar();
 
-        if (inputBuffer == 'q')
-        {
-            quit = true;
-        }
-        else
-        {
-            hasInput = true;
-        }
-    }
+		if (inputBuffer == 'q')
+		{
+			quit = true;
+		}
+		else
+		{
+			hasInput = true;
+		}
+	}
 }
 
 void DungeonEngine::handleInput()
 {
-    if(hasInput)
-    {
-        if(inputBuffer == 'm')
-        {
-            static int x = 1;
-            roomApi.showMessage("Hello There! " + std::to_string(x));
-            x++;
-        }
+	if (hasInput)
+	{
+		if (inCombat)
+		{
+			if (!combatManager->handleInput(inputBuffer))
+			{
+				inCombat = false;
+			}
+		}
+		else if (inputBuffer == 'm')
+		{
+			static int x = 1;
+			roomApi.showMessage("Hello There! " + std::to_string(x));
+			x++;
+		}
 
-        inputBuffer = 0;
-        hasInput = false;
-    }
+		inputBuffer = 0;
+		hasInput = false;
+	}
 }
 
 void DungeonEngine::render()
 {
-    //
-    // Clear the screen.
-    //
+	//
+	// Clear the screen.
+	//
 
 #ifdef _WIN32
-    system("cls");
+	system("cls");
 #else
-    system("clear");
+	system("clear");
 #endif
-    
-    std::cout << std::endl;
 
-    //
-    // Merge the tile maps.
-    //
+	std::cout << std::endl;
 
-    auto tileMap = rooms[currentRoomIndex]->getTileMap();
-    auto enemyList = roomApi.getEnemyList();
+	//
+	// Merge the tile maps.
+	//
 
-    for (int y = 0; y < tileMap.size(); y++)
-    {
-        for (int x = 0; x < tileMap[y].size(); x++)
-        {
-            for (auto enemy : enemyList)
-            {
-                if (enemy.x == x && enemy.y == y)
-                {
-                    tileMap[y][x] = enemy.glyph;
-                }
-            }
-        }
-    }
+	auto tileMap = rooms[currentRoomIndex]->getTileMap();
+	auto enemyList = roomApi.getEnemyList();
 
-    //
-    // Render the current room.
-    //
+	for (int y = 0; y < tileMap.size(); y++)
+	{
+		for (int x = 0; x < tileMap[y].size(); x++)
+		{
+			for (auto enemy : enemyList)
+			{
+				if (enemy.x == x && enemy.y == y)
+				{
+					tileMap[y][x] = enemy.glyph;
+				}
+			}
+		}
+	}
 
-    for(int y = 0; y < tileMap.size(); y++)
-    {
-        std::cout << tileMap[y] << std::endl;
-    }
+	//
+	// Render the current room.
+	//
 
-    std::cout << std::endl;
+	for (int y = 0; y < tileMap.size(); y++)
+	{
+		std::cout << tileMap[y] << std::endl;
+	}
 
-    //
-    // Render text logs.
-    //
+	std::cout << std::endl;
 
-    std::cout << "----------- Text Log -----------" << std::endl;
+	//
+	// Render text logs.
+	//
 
-    std::vector<std::string> messages = roomApi.getMessages();
+	std::cout << "----------- Text Log -----------" << std::endl;
 
-    for (int i = 4; i > 0; i--)
-    {
-        if(static_cast<int>(messages.size()) - i >= 0)
-        {
-            std::cout << messages[messages.size() - i] << std::endl;
-        }
-        else
-        {
-            std::cout << "" << std::endl;
-        }
-    }
+	std::vector<std::string> messages = roomApi.getMessages();
 
-    std::cout << "--------------------------------" << std::endl;
+	for (int i = 4; i > 0; i--)
+	{
+		if (static_cast<int>(messages.size()) - i >= 0)
+		{
+			std::cout << messages[messages.size() - i] << std::endl;
+		}
+		else
+		{
+			std::cout << "" << std::endl;
+		}
+	}
 
-    //
-    // Render player info.
-    //
+	std::cout << "--------------------------------" << std::endl;
 
-    std::cout << "----------- Player -------------" << std::endl;
+	//
+	// Render player info.
+	//
 
-    std::cout << "HP: " << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
+	std::cout << "----------- Player -------------" << std::endl;
 
-    std::cout << "--------------------------------" << std::endl;
+	std::cout << "HP: " << std::endl;
+	std::cout << "" << std::endl;
+	std::cout << "" << std::endl;
+	std::cout << "" << std::endl;
 
-    std::cout << std::endl;
+	std::cout << "--------------------------------" << std::endl;
 
-    //
-    // Render action menu.
-    //
+	std::cout << std::endl;
 
-    std::cout << "test msg: [m] | quit: [q] " << std::endl;
+	//
+	// Render action menu.
+	//
+
+	std::cout << "test msg: [m] | quit: [q] " << std::endl;
 }
 
 void DungeonEngine::loop()
 {
-    auto last = std::chrono::steady_clock::now();
+	auto last = std::chrono::steady_clock::now();
 
-    while(running)
-    {
-        auto now = std::chrono::steady_clock::now();
+	while (running)
+	{
+		auto now = std::chrono::steady_clock::now();
 
-        int delta = std::chrono::duration_cast<std::chrono::milliseconds>(now - last).count();
+		int delta = std::chrono::duration_cast<std::chrono::milliseconds>(now - last).count();
 
-        int ENGINE_FRAME_MILLISECONDS = 1000;
-        int ENGINE_SLEEP_TIME_MILLISECONDS = 100;
+		int ENGINE_FRAME_MILLISECONDS = 1000;
+		int ENGINE_SLEEP_TIME_MILLISECONDS = 100;
 
-        if(delta < ENGINE_FRAME_MILLISECONDS)
-        {
-            std::this_thread::sleep_for(std::chrono::milliseconds(ENGINE_SLEEP_TIME_MILLISECONDS));
-            continue;
-        }
+		if (delta < ENGINE_FRAME_MILLISECONDS)
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(ENGINE_SLEEP_TIME_MILLISECONDS));
+			continue;
+		}
 
-        last = now;
+		last = now;
 
-        handleInput();
+		handleInput();
 
-        if (quit)
-        {
-            running = false;
-            break;
-        }
-        
-        render();
-    }
+		if (quit)
+		{
+			running = false;
+			break;
+		}
+
+		render();
+	}
 }
 
